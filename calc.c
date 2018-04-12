1edb04a63411cef5d96216d86b8c230f4da4d0bf
Sebastian Velo <sebast.velo@gmail.com>
2018-04-12 19:23:28 -0300

    Me olvide esto

---
+++ calc.c
@@ -0,0 +1,9 @@
+/** PROTOTIPOS: */
+int calc_todasLasOperaciones(float operandoA, float operandoB);
+int calc_operacion(float operandoA, float operandoB, int opcion);
+int calc_factorial(int operando);
+
+float resultadoOperacion(float operandoA, float operandoB, int opcion);
+unsigned long resultadoFactorial(int operando);
+char simbolo(int opcion);
+
